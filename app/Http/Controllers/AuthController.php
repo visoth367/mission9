b38ba4125f6681b559ae6e27b9311acff5305c87
MacBook Air <macbookair@AIRY.local>
2024-06-30 20:05:54 +0700

    API done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signIn(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json(['token' => $token, 'user' => $user], 200);
+        }
+
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+}
